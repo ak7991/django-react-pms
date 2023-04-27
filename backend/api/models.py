b984e1d615f83924d90b7bd077fc03eb8e97b4ad
ak7991_myndsol <akshay.alok@myndsol.com>
2023-04-27 08:47:49 +0530

    UUID functionality Django

--- backend/api/models.py
+++ backend/api/models.py
@@ -30,27 +30,27 @@ class Incident(models.Model):
     # Create incident id according to spec
     def create_inc_id():
         prefix = "RMG"
-        five_random_nums = str(random.randint(99999)).ljust(5, "0")
-        current_year = datetime.now().year
+        five_random_nums = str(random.randint(0, 99999)).ljust(5, "0")
+        current_year = str(datetime.now().year)
         target = prefix + five_random_nums + current_year
         return target
     
     incident_id = models.CharField(max_length=12, unique=True, editable=False, default=create_inc_id)
-    date_created = models.DateField(auto_now_add=True)
+    date_created = models.DateField(auto_now_add=True, null=False, blank=False)
     description = models.TextField(max_length=128, null=True, blank=True)
     priority = models.CharField(max_length=20, null=True, choices=PRIORITY)
     user = models.ForeignKey(
         User, null=True, blank=True, on_delete=models.CASCADE)
     closed_status = models.BooleanField(null=False, default=False)
 
-    def close_incident(self):
-        if self.closed_status:
-            non_editable_fields = ["date_created", "description", "priority", "user"]
-            for field in non_editable_fields:
-                self.__getattribute__(field).editable = False
+    # def close_incident(self):
+    #     if self.closed_status:
+    #         non_editable_fields = ["date_created", "description", "priority", "user"]
+    #         for field in non_editable_fields:
+    #             self.__getattribute__(field).editable = False
 
     def __str__(self):
-        return str(self.name) + " - " + str(self.client)
+        return str(self.incident_id) + " - " + str(self.priority) + " - " + str(self.closed_status) + ' date: ' + datetime.strftime(self.date_created,"HH:MM:SS")
 
 
 # class Todolist(models.Model):
