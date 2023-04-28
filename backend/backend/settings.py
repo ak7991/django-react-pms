6d952916bfd5a696ee5f1437a2aea5f76623440e
Ubuntu <ubuntu@ip-172-31-95-193.ec2.internal>
2023-04-28 06:40:05 +0000

    cors setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -148,7 +148,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
        'http://localhost:3000',
 )
